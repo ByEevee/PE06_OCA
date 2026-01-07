9c09af2a080158220a7d703b68d4d5f40c2ba2ea
Ivan Carrasco <alu.ivan.carrasco@mataro.epiaedu.cat>
2026-01-07 22:58:44 +0100

    Quart Commit (FINAL), acabat de refinar errors, com saltar massa oques, etc..

--- jocOca.java
+++ jocOca.java
@@ -45,7 +45,7 @@ public class jocOca {
         
         boolean finalJoc = false;
 
-        do {
+        while (!finalJoc) {
             for (int i = 0; i < numJugadors; i++) {
                 int jugadorActual = ordreJugadors[i];
             
@@ -58,9 +58,11 @@ public class jocOca {
                 } 
 
                 else {
+                    System.out.println("Escriu 'tiro' per tirar els daus.");
                     boolean tiradaValida = false;
 
                     while (!tiradaValida) {
+                        
                         System.out.print(">> ");
                         String entrada = sc.nextLine().trim();
                         
@@ -88,7 +90,7 @@ public class jocOca {
 
                     
                     if (casellaJugadors[jugadorActual] == 0) {
-                        // Cas especial: primera tirada amb 3-6 (1+2)
+                        // Cas especial: primera tirada amb 3-6 
                         if ((dau1 == 1 && dau2 == 2) || (dau1 == 2 && dau2 == 1)) {
                             casellaJugadors[jugadorActual] = 26;
                             System.out.println("De dado a dado y tiro porque me ha tocado.");
@@ -98,7 +100,7 @@ public class jocOca {
                                 i--;
                             }
                         } 
-                        // Cas especial: primera tirada amb 4-5 (2+3)
+                        // Cas especial: primera tirada amb 4-5 
                         else if ((dau1 == 2 && dau2 == 3) || (dau1 == 3 && dau2 == 2)) {
                             casellaJugadors[jugadorActual] = 53;
                             System.out.println("De dado a dado y tiro porque me ha tocado.");
@@ -121,6 +123,7 @@ public class jocOca {
                                 System.out.println("Avances fins la casella 63!");
                                 finalDelJoc(jugadorActual);
                                 finalJoc = true;
+                                break;
                             } else {
                                 System.out.println("Avances fins la casella " + novaCasella + ".");
                                 boolean tornExtra = processarCasella(jugadorActual);
@@ -143,6 +146,7 @@ public class jocOca {
                             System.out.println("Avances fins la casella 63!");
                             finalDelJoc(jugadorActual);
                             finalJoc = true;
+                            break; 
                         } else {
                             System.out.println("Avances fins la casella " + novaCasella + ".");
                             boolean tornExtra = processarCasella(jugadorActual);
@@ -155,7 +159,7 @@ public class jocOca {
                     
                 
             }
-        } while (!finalJoc);
+        } 
         
         sc.close();
     }
@@ -179,8 +183,8 @@ public class jocOca {
                     ordreJugadors[j] = ordreTemp;
                 }
             }
-        }
-        
+        }  //doble for per ordenar l'array ordreJugadors
+
         System.out.println("\nOrdre de joc:");
         for (int i = 0; i < numJugadors; i++) {
             System.out.println((i + 1) + ". " + nomJugadors[ordreJugadors[i]]);
@@ -270,31 +274,28 @@ public class jocOca {
         
         if (casella == 5 || casella == 9 || casella == 14 || casella == 18 || 
             casella == 23 || casella == 27 || casella == 32 || casella == 36 || 
-            casella == 41 || casella == 45 || casella == 50 || casella == 54 || casella == 59) // Caselles d'Oca
+            casella == 41 || casella == 45 || casella == 50 || casella == 54 || casella == 59)
         {
-            
             System.out.println("Casella nº " + casella + ": Oca. De oca en oca y tiro porque me toca.");
             
             int seguentOca = trobarSeguentOca(casella);
             if (seguentOca != -1) {
                 casellaJugadors[jugador] = seguentOca;
                 System.out.println("Avances fins la casella " + seguentOca + ".");
-                tornExtra = processarCasella(jugador);
-                tornExtra = true;
             }
+            tornExtra = true;  
         } else if (casella == 6) {
             System.out.println("Casella nº 6: Pont. De puente a puente y tiro porque me lleva la corriente.");
             casellaJugadors[jugador] = 12;
             System.out.println("Avances fins la casella 12.");
-            tornExtra = processarCasella(jugador);
             tornExtra = true;
         } else if (casella == 12) {
             System.out.println("Casella nº 12: Pont. De puente a puente y tiro porque me lleva la corriente.");
             casellaJugadors[jugador] = 6;
             System.out.println("Avances fins la casella 6.");
-            tornExtra = processarCasella(jugador);
             tornExtra = true;
-        } else if (casella == 19) {
+        }
+         else if (casella == 19) {
             System.out.println("Casella nº 19: Fonda. Una jugada sense moure's.");
             tornsBloquejats[jugador] = 1;
         } else if (casella == 31) {
@@ -303,6 +304,7 @@ public class jocOca {
         } else if (casella == 42) {
             System.out.println("Casella nº 42: Laberint. Tornes a la casella 39.");
             casellaJugadors[jugador] = 39;
+            System.out.println("Ara estàs a la casella 39.");
         } else if (casella == 52) {
             System.out.println("Casella nº 52: Presó. Tres torns sense moure's.");
             tornsBloquejats[jugador] = 3;
