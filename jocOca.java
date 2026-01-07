a31b206b637401975ad2d76e03a4c990fbd0c2c2
Ivan Carrasco <alu.ivan.carrasco@mataro.epiaedu.cat>
2026-01-07 14:19:43 +0100

    Segon commit, Arreglat alguns errors + Sistema de ordre de jugadors + Inicialitzat varies posibles variables per al futur

--- jocOca.java
+++ jocOca.java
@@ -4,61 +4,83 @@ import java.util.Scanner;
 public class jocOca {
     static Scanner sc = new Scanner(System.in);
     int numJugadors = 0;
-    int [] casellaJugadors = new int [numJugadors];
-    String [] nomJugadors = new String [numJugadors];
-    int [] ordreTirades = new int [numJugadors];
+    int[] casellaJugadors;
+    String[] nomJugadors;
+    int[] tornsBloquejats;
+    int[] ordreJugadors;
     
     public static void main(String[] args) {
         jocOca joc = new jocOca();
         joc.jocPrincipal();
     }
+    
     public void jocPrincipal() {
         System.out.println("=========================================");
         System.out.println("       Benvinguts al joc de l'Oca!       ");
         System.out.println("=========================================");
         System.out.println();
+        
+        do {
+            System.out.println("Quants jugadors hi ha? (2-4)");
+            numJugadors = llegirEnter();
+        } while (numJugadors < 2 || numJugadors > 4);
+        
+        casellaJugadors = new int[numJugadors];
+        nomJugadors = new String[numJugadors];
+        tornsBloquejats = new int[numJugadors];
+        ordreJugadors = new int[numJugadors];
+        
+        for (int i = 0; i < numJugadors; i++) {
+            casellaJugadors[i] = 0;
+            tornsBloquejats[i] = 0;
+        }
+        
+        for (int i = 0; i < numJugadors; i++) {
+            System.out.println("Nom del jugador " + (i + 1));
+            nomJugadors[i] = llegirLletres();
+        }
+        
+        System.out.println("\nComença el joc!");
+        determinarOrdre();
+        
         boolean finalJoc = false;
         do {
-            System.out.println("Quants jugadors hi ha?");
-            numJugadors = llegirEnter();
-            
             for (int i = 0; i < numJugadors; i++) {
-                //Inicialitzem totes les posicions dels jugadors a la casella 0
-                casellaJugadors[i] = 0;
+                System.out.println("\n En procés...");
+                    
+                
             }
-            for (int i = 0; i < numJugadors; i++) {
-                //Posem nom a tots els jugadors
-                System.out.println("Nom del jugador " + (i + 1));
-                nomJugadors[i] = llegirLletres();
-            }
-            System.out.println("Comença el joc!");
-            System.out.println("Determinem el ordre dels jugadors llençant el dau...");
-            ordreJugadors();
-            
-        } while (finalJoc);
+        } while (!finalJoc);
         
-        
-
         finalDelJoc();
     }
-    public void ordreJugadors() {
-        System.out.println("Tirant el dau per determinar l'ordre dels jugadors...");
-        int [] resultatTirades = new int [numJugadors];
+    public void determinarOrdre() {
+        System.out.println("\nDeterminem l'ordre dels jugadors llençant el dau...");
+        int[] tirades = new int[numJugadors];
+        
         for (int i = 0; i < numJugadors; i++) {
-            int tirada = tirarDau();
-            System.out.println("El jugador " + nomJugadors[i] + " ha tret un " + tirada);
-            resultatTirades[i] = tirada;
+            tirades[i] = tirarDau();
+            ordreJugadors[i] = i;
+            System.out.println("El jugador " + nomJugadors[i] + " ha tret un " + tirades[i]);
+            System.out.println("Prem enter per continuar...");
+            sc.nextLine();
         }
+        
+        for (int i = 0; i < numJugadors - 1; i++) {
+            for (int j = i + 1; j < numJugadors; j++) {
+                if (tirades[ordreJugadors[j]] > tirades[ordreJugadors[i]]) {
+                    int ordreTemp = ordreJugadors[i];
+                    ordreJugadors[i] = ordreJugadors[j];
+                    ordreJugadors[j] = ordreTemp;
+                }
+            }
+        }
+        
+        System.out.println("\nOrdre de joc:");
         for (int i = 0; i < numJugadors; i++) {
-            if (resultatTirades[i]>resultatTirades[i+1]|| resultatTirades [i] > resultatTirades[ i+2]) {
-                ordreTirades[i]=1;
-            }
-            else if (resultatTirades [i] < resultatTirades[ i-1] || resultatTirades [i] < resultatTirades[ i-2]) {
-                ordreTirades[i]=1;
-            }
+            System.out.println((i + 1) + ". " + nomJugadors[ordreJugadors[i]]);
         }
-            
-        }
+    }  
 
     
     public int tirarDau() {
@@ -114,29 +136,24 @@ public class jocOca {
         }
         return entrada;
     }
-    public String llegirLletres() {
+   public String llegirLletres() {
         boolean entradaCorrecta = false;
         String entrada = "";
+
         while (!entradaCorrecta) {
-            try {
-                entrada = sc.nextLine().trim();
-                if (!entrada.matches("[a-zA-Z]+")) {
-                    throw new InputMismatchException();
-                }
-                else if (entrada.equals("")) {
-                    System.out.println("No es permeten espais buits. Si us plau, introdueix un nom vàlid.");
-                }
-                else{
-                    entradaCorrecta = true;
-                }
-                
-            }catch (Exception e) {
-                System.out.println("S'ha produït un error inesperat. Si us plau, torna-ho a intentar.");
-            } finally {
-                sc.nextLine();
+            entrada = sc.nextLine().trim();
+
+            if (entrada.isEmpty()) {
+                System.out.println("(!) Error: no es permet deixar el camp buit.");
+            }
+            else if (!entrada.matches("[a-zA-Z]+")) {
+                System.out.println("(!) Error: només es permeten lletres, sense números ni espais.");
+            }
+            else {
+                entradaCorrecta = true;
             }
         }
+
         return entrada;
-    
     }
 }
